a90d8d5234d9abe5343cdb200d2a060be4cfd191
Wesley Schwartz <wesleyschwartz@hotmail.com>
2022-04-20 15:29:44 -0300

    projeto iniciado

---
+++ src/main/java/br/com/compass/ApiUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUdemyApplication.class, args);
+	}
+
+}
