b376394176b816fe4ed2f25c389515d7832f3566
Wesley Schwartz <wesleyschwartz@hotmail.com>
2022-04-20 17:35:12 -0300

    class UserDTO

---
+++ src/main/java/br/com/compass/domain/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package br.com.compass.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
